69e6c84849ab3c53d68e35ce6d18ff158b36718c
Янина Бурова <yaninaburova@gmail.com>
2020-11-14 17:59:25 +0800

    new tests

--- ExtHelper/ExtMeth.cs
+++ ExtHelper/ExtMeth.cs
@@ -2,8 +2,18 @@
 
 namespace ExtHelper
 {
+    /// <summary>
+    /// Класс ExtMeth, включает в себя 2 метода
+    /// </summary>
     public static class ExtMeth
     {
+        /// <summary>
+        /// Метод определения конкретного бита
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="position"></param>
+        /// <param name="size"></param>
+        /// <returns>е</returns>
         public static bool GetBits(this int data, int position, int size)
         {
             if (position < 0 || position > size)
--- ExtHelperTests/ExtMethTests.cs
+++ ExtHelperTests/ExtMethTests.cs
@@ -9,16 +9,28 @@ namespace ExtHelper.Tests
     [TestClass()]
     public class ExtMethTests
     {
+       
+        int w = 0b11001010;
+
         [TestMethod()]
         public void GetBitsTest()
         {
-            Assert.Fail();
+            bool expected = true;
+            bool actual= w.GetBits(1, 2);
+                       
+            Assert.AreEqual(expected, actual);
+        
         }
 
         [TestMethod()]
         public void SetBitsTest()
         {
-            Assert.Fail();
+       
+            int act = w.SetBits(1, 0, 7);
+            int exp = 0b11001011;
+
+            Assert.AreEqual(exp, act);
         }
+   
     }
 }
