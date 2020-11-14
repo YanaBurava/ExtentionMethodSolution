6cd9ab4fb03cf5a87fcefef990427859b0c48269
Янина Бурова <yaninaburova@gmail.com>
2020-11-14 11:44:00 +0800

    ver 1.0

---
+++ ExtHelper/ExtMeth.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ExtHelper
+{
+    public static class ExtMeth
+    {
+        public static bool GetBits(this int data, int position, int size)
+        {
+            if (position < 0 || position > size)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            int bit = (data >> position) & 1;
+            return Convert.ToBoolean(bit);
+        }
+
+        public static int SetBits(this ref int data, int bit, int position, int size)
+        {
+            if (position < 0 || position > size)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            if (bit == 1)
+            {
+                return data = data | 1 << position;
+            }
+            else
+            {
+                return data = data & ~(1 << position);
+            }
+
+        }
+    }
+}
---
+++ ExtMethod.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExtHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int word = 0b10001100;
+
+            Console.WriteLine(Convert.ToString(word, 2));
+            Console.WriteLine(word.GetBits(1, 7));
+            Console.WriteLine(Convert.ToString(word.SetBits(1, 1, 7), 2));
+            Console.WriteLine(word.GetBits(1, 7));
+            //Console.WriteLine(Convert.ToString(word.SetBits(0, 3), 2));
+            //Console.WriteLine(word.GetBits(3));
+            //Console.WriteLine("----------");
+            //for (int i = 0; i <=7; ++i)
+            //Console.WriteLine(word.GetBits(i));
+        }
+    }
+}
