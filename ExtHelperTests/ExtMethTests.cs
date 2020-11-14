b8a371615b53b63c18654f842ca2745aecb6f6e3
Янина Бурова <yaninaburova@gmail.com>
2020-11-14 11:54:10 +0800

    Add tests

---
+++ ExtHelperTests/ExtMethTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExtHelper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtHelper.Tests
+{
+    [TestClass()]
+    public class ExtMethTests
+    {
+        [TestMethod()]
+        public void GetBitsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetBitsTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
